3440112c39688985d6c8e5f1ba970c992386ac88
Andrew Styles <andystyles@hotmail.co.uk>
2023-01-27 20:03:34 +0000

    when button is clicked to search the time 3 hours from now is displayed

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 let urlStart = "http://api.openweathermap.org/geo/1.0/direct?q=";
 let locationInput = document.getElementById("search-input");
 let LimitApiKey = "&limit=5&appid=6f754f65b6008bfea31fb1bd4a90e6ba";
+let today = document.querySelector("#today")
 
 document.querySelector("#search-button").addEventListener("click", function(event){
     event.preventDefault();
@@ -26,8 +27,18 @@ document.querySelector("#search-button").addEventListener("click", function(even
     .then(data => {
         
         console.log(data);
-        
+        let cityToday = document.createElement("div")
+        cityToday.innerHTML = 
+        `<h1>${moment(data.list[0].dt, "X").format("DD/MM/YYYY HH:mm:ss")}</h1>
+        <img src="http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png">
+        <h3>Temp: ${data.list[0].main.temp} °C</h3>
+        <h3>Wind: ${data.list[0].wind.speed} KPH</h3>
+        <h3>Humidity: ${data.list[0].main.humidity} %</h3>
+        `
+        today.append(cityToday)
     })
 })
-
+function createCard() {
+    
+}
 // event.target.textContent to grab text of city button
